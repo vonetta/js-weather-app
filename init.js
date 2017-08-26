57e393e1264666cd62fe6ea923b5c97db92ab584
Vonetta <vonettastevenson@gmail.com>
2017-08-26 14:46:16 -0700

    completed udemy course weather app

---
+++ init.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var searchButton = document.querySelector('button');
+var searchCity = document.querySelector('#city');
+
+var loadingText = document.querySelector('#load');
+var weatherBox = document.querySelector('#weather');
+
+var weatherCity = document.querySelector('#weatherCity');
+var weatherDescription = document.querySelector('#weatherDescription');
+var weatherTemperature = document.querySelector('#weatherTemperature');
